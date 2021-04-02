2a219f88700ab774f8188fe77f54edfff6c60790
MegaPixel <devsnowflake107@gmail.com>
2021-04-02 09:24:27 +0545

    another command

--- index.js
+++ index.js
@@ -5,6 +5,25 @@ const fetch = require("node-fetch");
 const client = new Client();
 const PREFIX = "a!";
 
+const ACTIVITIES = {
+    "poker": {
+        id: "755827207812677713",
+        name: "Poker Night"
+    },
+    "betrayal": {
+        id: "773336526917861400",
+        name: "Betrayal.io"
+    },
+    "youtube": {
+        id: "755600276941176913",
+        name: "YouTube Together"
+    },
+    "fishington": {
+        id: "814288819477020702",
+        name: "Fishington.io"
+    }
+};
+
 client.on("ready", () => console.log("Bot is online!"));
 client.on("warn", console.warn);
 client.on("error", console.error);
@@ -45,6 +64,39 @@ client.on("message", async message => {
             })
             .catch(e => {
                 message.channel.send("❌ | Could not start **YouTube Together**!");
+            })
+    }
+    
+    // or use this
+    if (cmd === "activity") {
+        const channel = message.guild.channels.cache.get(args[0]);
+        if (!channel || channel.type !== "voice") return message.channel.send("❌ | Invalid channel specified!");
+        if (!channel.permissionsFor(message.guild.me).has("CREATE_INSTANT_INVITE")) return message.channel.send("❌ | I need `CREATE_INSTANT_INVITE` permission");
+        const activity = ACTIVITIES[args[1] ? args[1].toLowerCase() : null];
+        if (!activity) return message.channel.send(`❌ | Correct formats:\n${Object.keys(ACTIVITIES).map(m => `- **${PREFIX}activity <Channel_ID> ${m}**`).join("\n")}`);
+
+        fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
+            method: "POST",
+            body: JSON.stringify({
+                max_age: 86400,
+                max_uses: 0,
+                target_application_id: activity.id,
+                target_type: 2,
+                temporary: false,
+                validate: null
+            }),
+            headers: {
+                "Authorization": `Bot ${client.token}`,
+                "Content-Type": "application/json"
+            }
+        })
+            .then(res => res.json())
+            .then(invite => {
+                if (!invite.code) return message.channel.send(`❌ | Could not start **${activity.name}**!`);
+                message.channel.send(`✅ | Click here to start **${activity.name}** in **${channel.name}**: <https://discord.gg/${invite.code}>`);
+            })
+            .catch(e => {
+                message.channel.send(`❌ | Could not start **${activity.name}**!`);
             })
     }
 });
